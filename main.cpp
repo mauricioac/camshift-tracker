25c42e632eb35c4c81bfb437c1c7b926f1f5aa12
Maurício André Cinelli <mauricio.andre.cinelli@gmail.com>
2016-05-22 22:31:09 -0300

    Ta indo

--- main.cpp
+++ main.cpp
@@ -12,57 +12,148 @@
 using namespace std;
 using namespace cv;
 
-// class Objeto {
-//   Mat roi;
+class Objeto {
+public:
 
-//   void track(Mat image) {
+  Mat regiao;
+  Mat frame;
+  Mat fbproj;
+  Mat fhsv;
+  Mat fbthr;
+  Rect wind;
+  MatND rhist;
+  Mat mask;
+  Mat rhsv;
+  int framesVivo;
 
-//   }
-// };
+  Objeto(Mat imagem, Rect r) {
+    cvtColor(imagem(r), rhsv, CV_BGR2HSV);
 
-void trackROI(VideoCapture * cap, Point2f tl, Point2f br, Mat roi) {
-
-    Mat rhsv;
-    rhsv = roi.clone();
-    cvtColor(roi, rhsv, CV_BGR2HSV);
-    Mat mask;
-
-    cvNamedWindow("BackProjeção",CV_WINDOW_AUTOSIZE);
-    cvNamedWindow( "Tracking", CV_WINDOW_AUTOSIZE );
-
-    MatND rhist;
     int rhistsz = 180;    // bin size
     float range[] = { 0, 180};
     const float *ranges[] = { range };
     int channels[] = {0};
+    framesVivo = 0;
 
     calcHist( &rhsv, 1, channels, Mat(), rhist, 1, &rhistsz, ranges, true, false );
     normalize(rhist,rhist,0,255,NORM_MINMAX, -1, Mat() );
 
-    Mat frame;
-    Mat fbproj;
-    Mat fhsv;
-    Mat fbthr;
-    Rect wind;
+    wind = r;
+    wind.x += 8;
+    wind.y += 8;
+    wind.width -= 8;
+    wind.height -= 8;
+  }
 
+  void track(Mat &crop, Mat &image) {
+    float range[] = { 0, 180};
+    const float *ranges[] = { range };
+    int channels[] = {0};
 
-    wind.x=tl.x;wind.y=tl.y;wind.height=br.y - tl.y;wind.width=br.x - tl.x;
+    cvtColor(crop,fhsv,CV_BGR2HSV);
+    // imshow("aaaa", fhsv);
+    // waitKey(2000);
+    calcBackProject(&fhsv,1,channels,rhist,fbproj,ranges,1,true);
+    // imshow("aaaa", fbproj);
+    // waitKey(2000);
+    threshold(fbproj, fbthr, 220, 1,CV_THRESH_TOZERO);
+    normalize(fbthr,fbthr,0,255,NORM_MINMAX, -1, Mat() );
+    // imshow("aaaa", fbthr);
+    // waitKey(2000);
+    RotatedRect rect = CamShift(fbthr,wind,TermCriteria(CV_TERMCRIT_EPS | CV_TERMCRIT_ITER, 5, 0.1));
+    wind = rect.boundingRect();
 
-    while (true) {
-
-        cap->read(frame);
-        if(frame.empty())
-            return;
-        cvtColor(frame,fhsv,CV_BGR2HSV);
-        calcBackProject(&fhsv,1,channels,rhist,fbproj,ranges,1,true);
-        normalize(fbproj,fbproj,0,255,NORM_MINMAX, -1, Mat() );
-        threshold(fbproj, fbthr, 220, 1,CV_THRESH_TOZERO);
-        imshow("BackPprojeção", fbthr);
-        meanShift(fbthr,wind,TermCriteria(CV_TERMCRIT_EPS | CV_TERMCRIT_ITER, 10, 1));
-        rectangle(frame, wind, CV_RGB(255,255,255),1);
-        imshow("Tracking", frame);
-        waitKey(1);
+    Point2f vertices[4];
+    rect.points(vertices);
+    for (int i = 0; i < 4; i++) {
+      line(image, vertices[i], vertices[(i+1)%4], Scalar(0,255,0));
     }
+
+    framesVivo++;
+  }
+};
+
+Mat img, imgtemp;
+Rect roi;
+Point p1, p2, p3;
+ bool
+  flag1 = false,
+  flag2 = false,
+  flag3 = false,
+  flag4 = true;
+
+void on_mouse( int event, int x, int y, int flags, void* param )
+{
+
+  if (!flag3)
+    return;
+
+  switch(event)
+  {
+    case CV_EVENT_LBUTTONDOWN:
+        if (!flag2) {
+          p3.x = x;
+          p3.y = y;
+          flag1 = true;
+        }
+        break;
+    case CV_EVENT_MOUSEMOVE:
+        imgtemp = img.clone();
+        if (flag1) {
+                    rectangle(imgtemp,p3, Point(x,y+5),  CV_RGB(255,255,255),1);
+        }
+        imshow( "Imagem", imgtemp);
+        break;
+    case CV_EVENT_LBUTTONUP:
+        p1.x = p3.x;
+        p1.y = p3.y;
+        p2.x = x;
+        p2.y = y;
+        flag1 = false;
+        flag2 = true;
+        break;
+    default:
+      break;
+  }
+}
+
+bool pegaROI() {
+
+  imgtemp = img.clone();
+
+  if(img.empty())
+     return -1;
+
+  flag1 = flag2 = false;
+  flag3 = true;
+
+  p1.x = p1.y = 10;
+  p2.x = img.cols-10;
+  p2.y = img.rows-10;
+
+
+  cvNamedWindow( "Imagem", CV_WINDOW_AUTOSIZE );
+  setMouseCallback("Imagem", on_mouse, 0 );
+  imshow( "Imagem", img);
+
+  if (img.rows< 50 || img.cols < 50) {
+      printf("Erro: imagem ḿuito pequena!\n");
+      return -1;
+  }
+  imgtemp = img.clone();
+
+  while(!flag2) {
+    if(waitKey(10) == 27) {
+      break;
+    }
+    if (flag2) {
+      break;
+    }
+  }
+
+  cvDestroyWindow("Imagem");
+    roi = Rect(p1,p2);
+    return true;
 }
 
 int main(int argc, char *argv[]) {
@@ -72,16 +163,19 @@ int main(int argc, char *argv[]) {
   if (argc < 2)
   {
       stream = true;
-      printf("Aviso: Faltou arquivo de video!\n");
-      printf("       Usando Stream da Camera!\n");
   }
 
-  VideoCapture cap(0);
+  VideoCapture *cap;
 
-  if(!cap.isOpened())
+  if (stream) {
+    cap = new VideoCapture(0);
+  } else {
+    cap = new VideoCapture(argv[1]);
+  }
+
+  if(!cap->isOpened())
       return -1;
 
-  namedWindow("aaaa", WINDOW_AUTOSIZE);
   Mat frame;
 
   Ptr< BackgroundSubtractorMOG2> mog2 = createBackgroundSubtractorMOG2(500,60,true);
@@ -90,18 +184,35 @@ int main(int argc, char *argv[]) {
   mog2->setBackgroundRatio(0.001);
   Mat binaryImg;
 
-  cap >> frame;
-  mog2->apply(frame, mascara_background);
+  for (int i = 0; i < 80; i++) {
+    *cap >> frame;
+    mog2->apply(frame, mascara_background);
+  }
+
+  img = frame.clone();
+  namedWindow("aaaa", WINDOW_AUTOSIZE);
+
+  Mat elemento = getStructuringElement(MORPH_RECT, Size(3, 7), Point(1,3) );
+
+  pegaROI();
+
+  int contador = 0;
+
+  vector<Objeto> objetos;
 
   while (true) {
 
-    cap >> frame;
+    *cap >> frame;
     if(frame.empty()) {
       break;
     }
 
     // remove sombras
     threshold(mascara_background, binaryImg, 50, 255, CV_THRESH_BINARY);
+
+    // for (int i = 0; i < 10; i++)
+    //   morphologyEx(binaryImg, binaryImg, CV_MOP_CLOSE, elemento);
+
     Mat binOrig = binaryImg.clone();
 
     mog2->apply(frame, mascara_background);//,-0.5);
@@ -110,11 +221,83 @@ int main(int argc, char *argv[]) {
 
     vector< vector<Point> > contornos;
     findContours(ContourImg, contornos, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
+    Mat mask = Mat::zeros(ContourImg.rows, ContourImg.cols, CV_8UC3);
+    drawContours(mask, contornos, -1, Scalar(255), CV_FILLED);
+
+    Mat mask2 = Mat::zeros(ContourImg.rows, ContourImg.cols, CV_8UC3);
+    drawContours(mask2, contornos, -1, Scalar(255), CV_FILLED);
+
+    Mat crop(frame.rows, frame.cols, frame.type());
+    crop.setTo(Scalar(255,255,255));
+    frame.copyTo(crop, mask);
+
+    Mat crop2(frame.rows, frame.cols, frame.type());
+    frame.copyTo(crop2, mask2);
 
     for (int i = 0; i < (int) contornos.size(); i++) {
       Rect bb = boundingRect(contornos[i]);
-      rectangle(frame, bb, Scalar(255, 0, 128), 2);
+
+      if (bb.width <= 10 || bb.height <= 10)
+        continue;
+
+      bool achou = false;
+      for (int j = 0; j < objetos.size(); j++) {
+        Rect intersecao = bb & objetos[j].wind;
+
+        if (intersecao.height > 7 || intersecao.width > 10) {
+          achou = true;
+        }
+      }
+
+      if (achou) {
+        continue;
+      }
+
+        // rectangle(frame, bb, Scalar(255, 0, 128), 2);
+      if (bb.y >= roi.y && bb.y <= roi.y + 10) {
+        // cout << "novo?" << endl;
+        // imshow("aaaa", crop);
+        // waitKey(2000);
+        Objeto o(crop, bb);
+        objetos.push_back(o);
+      }
     }
+
+    // rectangle(frame, roi, Scalar(255, 0, 0), 2);
+
+    vector<Objeto> novos_objetos;
+
+    if (objetos.size() > 0) {
+
+      // imshow("aaaa", crop);
+      // waitKey(4000);
+    }
+
+    for (int i = 0; i < (int) objetos.size(); i++) {
+      objetos[i].track(crop, frame);
+
+      if (objetos[i].wind.y > roi.y + roi.height + 5) {
+        contador += 1;
+        continue;
+      }
+
+      if (objetos[i].wind.height > 90 || objetos[i].wind.width > 120)
+      {
+        continue;
+      }
+
+      novos_objetos.push_back(objetos[i]);
+    }
+
+    objetos = novos_objetos;
+
+    string text = "Contador: " + to_string(contador);
+    int fontFace = FONT_HERSHEY_SIMPLEX;
+    double fontScale = 0.7;
+    int thickness = 2;
+    cv::Point textOrg(10, 30);
+
+    putText(frame, text, textOrg, fontFace, fontScale, Scalar(0, 0, 255), thickness,8);
 
     imshow("aaaa", frame);
 
